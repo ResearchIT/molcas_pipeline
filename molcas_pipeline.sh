8b23d8054ab87cb9c305c4196680000c42844654
Levi Baber <baberlevi@gmail.com>
2017-04-26 11:28:38 -0500

    fixing braces

--- molcas_pipeline.sh
+++ molcas_pipeline.sh
@@ -17,7 +17,7 @@ do
         esac
 done
 
-DOWNSTREAM = $UPSTREAM/Trajectory_{$STEP}fs
+DOWNSTREAM=$UPSTREAM/Trajectory_${STEP}fs
 
 if [ ! -d "$UPSTREAM" ]; then
   echo ERROR: Upstream folder path is incorrect
@@ -25,12 +25,12 @@ fi
 
 mkdir $UPSTREAM/$DOWNSTREAM
 
-cp $UPSTREAM/*.input $UPSTREAM/$DOWNSTREAM/Trajectory_{$STEP}fs.input
-cp $UPSTREAM/*.prm $UPSTREAM/$DOWNSTREAM/Trajectory_{$STEP}fs.prm
-cp $UPSTREAM/*.key $UPSTREAM/$DOWNSTREAM/Trajectory_{$STEP}fs.key
+cp $UPSTREAM/*.input $UPSTREAM/$DOWNSTREAM/Trajectory_${STEP}fs.input
+cp $UPSTREAM/*.prm $UPSTREAM/$DOWNSTREAM/Trajectory_${STEP}fs.prm
+cp $UPSTREAM/*.key $UPSTREAM/$DOWNSTREAM/Trajectory_${STEP}fs.key
 
-sed -i 's/{$UPSTREAM}*.prm/Trajectory_{$STEP}fs.prm/g' $UPSTREAM/$DOWNSTREAM/Trajectory_{$STEP}fs.key
+sed -i 's/{$UPSTREAM}*.prm/Trajectory_${STEP}fs.prm/g' $UPSTREAM/$DOWNSTREAM/Trajectory_${STEP}fs.key
 
 #might want to consider changing the job name in each of the sbatch scripts, or make a sbatch array
 cp $UPSTREAM/molcas_sub $UPSTREAM/$DOWNSTREAM/molcas_sub
-sed -i 's/file_name/Trajectory_{$STEP}fs/g' $UPSTREAM/$DOWNSTREAM/molcas_sub
+sed -i 's/file_name/Trajectory_${STEP}fs/g' $UPSTREAM/$DOWNSTREAM/molcas_sub
